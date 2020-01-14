2f8236e3d70f6353de5f2748ce82c7977e50ab1a
paydaylight <maxat235@gmail.com>
2020-01-14 22:45:24 +0600

    updates

--- parser.py
+++ parser.py
@@ -1,11 +1,28 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
-page = urlopen("https://matheo.uliege.be/handle/2268.2/6800")
-
-soup = BeautifulSoup(page, 'html.parser')
-
 # print(soup.find_all('a', {'class': 'btn btn-primary hidden-print'}))
 
-for link in soup.find_all('a', {'class': 'btn btn-primary hidden-print'}):
-    print(link.get('href'), link.get_text())
+
+def parse(page_num):
+    page = urlopen(f"https://matheo.uliege.be/handle/2268.2/{page_num}")
+    soup = BeautifulSoup(page, 'html.parser')
+
+    for link in soup.find_all('a', {'class': 'btn btn-primary hidden-print'}):
+        if link.get_text() == 'View/Open':
+            print(link.get('href'), link.get_text())
+            # create directory, download file
+        else:
+            # break execution of current page
+            print('restricted/forbidden')
+            return
+
+    # extract metadata about master thesis
+
+    for metadata in list(zip(soup.find_all('td', {'class': 'metadataFieldLabel'}),
+                             soup.find_all('td', {'class': 'metadataFieldValue'}))):
+        print(f"{metadata[0].get_text()} - {metadata[-1].get_text()}")
+    # if page_num < 7888:
+    #     parse(page_num+1)
+
+
+parse(1814)
