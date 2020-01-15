a5a11c8137e55a399fb5780e323d304a6a2b7370
paydaylight <maxat235@gmail.com>
2020-01-15 10:49:26 +0600

    added files download

--- parser.py
+++ parser.py
@@ -1,28 +1,69 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-# print(soup.find_all('a', {'class': 'btn btn-primary hidden-print'}))
+import os
+import requests
+
+# constants
+DOWNLOADS = 'downloads'
+
+
+def extract_name(soup):
+    return soup.find('div', {'class': 'col-md-11'}).h5.strong.get_text()
+
+
+def make_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def download_file(path, url, mode='wb'):
+    if not os.path.exists(path):
+        open(path, mode).write(requests.get(url).content)
+
+
+def create_file(path):
+    if not os.path.exists(path):
+        open(path, 'wra+')
 
 
 def parse(page_num):
     page = urlopen(f"https://matheo.uliege.be/handle/2268.2/{page_num}")
     soup = BeautifulSoup(page, 'html.parser')
+    has_access = False
 
-    for link in soup.find_all('a', {'class': 'btn btn-primary hidden-print'}):
-        if link.get_text() == 'View/Open':
-            print(link.get('href'), link.get_text())
-            # create directory, download file
-        else:
-            # break execution of current page
-            print('restricted/forbidden')
-            return
+    download_buttons = soup.find_all('a', {'class': 'btn btn-primary hidden-print'})
+    file_names = soup.find_all('a', {'class': 'bitstream'})
 
-    # extract metadata about master thesis
+    for link in list(zip(download_buttons, file_names)):
+        if link[0].get_text() == 'View/Open':
+            person_name = extract_name(soup)
+            print(f"parsing page about {person_name}, page: {page}")
 
-    for metadata in list(zip(soup.find_all('td', {'class': 'metadataFieldLabel'}),
-                             soup.find_all('td', {'class': 'metadataFieldValue'}))):
-        print(f"{metadata[0].get_text()} - {metadata[-1].get_text()}")
+            # create named directory
+            make_dir(f"{DOWNLOADS}/{person_name}")
+
+            # download file (resume or thesis)
+            download_file(f"{DOWNLOADS}/{person_name}/{link[-1].get_text()}", f"https://matheo.uliege.be/{link[-1].get('href')}")
+
+            # mark to continue parsing
+            has_access = True
+
+    # extract metadata about master thesis if thesis is open to view
+    if has_access:
+        labels = soup.find_all('td', {'class': 'metadataFieldLabel'})
+        values = soup.find_all('td', {'class': 'metadataFieldValue'})
+        for metadata in list(zip(labels, values)):
+            print(f"{metadata[0].get_text()} - {metadata[-1].get_text()}")
+
+        create_file(f"{DOWNLOADS}/data.csv")
+        with open(f"{DOWNLOADS}/data.csv", 'a') as doc:
+            print(doc)
+        #  fd.write(myCsvRow)
+
+    # iterate forward
     # if page_num < 7888:
     #     parse(page_num+1)
 
 
-parse(1814)
+# make_dir(DOWNLOADS)
+parse(6800)
