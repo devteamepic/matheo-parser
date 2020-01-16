a9f7f5fb6a2b71a58084490638e414e49acbc318
paydaylight <maxat235@gmail.com>
2020-01-16 10:54:52 +0600

    added prints for every field, almost done

--- parser.py
+++ parser.py
@@ -2,9 +2,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os
 import requests
-
+import csv
+import re
 # constants
 DOWNLOADS = 'downloads'
+COLUMN_NAMES = ['Page', 'Title', 'Translated title', 'Date of defense', 'Advisor(s)',
+                "Committee's member(s)", 'Funders', 'Author', 'Language', 'Number of pages',
+                'Keywords', 'Target public', 'Discipline(s)', 'Institution(s)', 'Degree', 'Faculty',
+                'Total number of views', 'Total number of downloads', 'Abstract']
 
 
 def extract_name(soup):
@@ -21,9 +26,21 @@ def download_file(path, url, mode='wb'):
         open(path, mode).write(requests.get(url).content)
 
 
-def create_file(path):
+def create_file(path=f"{DOWNLOADS}/data.csv"):
     if not os.path.exists(path):
-        open(path, 'wra+')
+        open(path, 'w+')
+
+
+def append_row(dict_rows, csv_path=f"{DOWNLOADS}/data.csv"):
+    try:
+        with open(csv_path, 'a') as csv_doc:
+            csv_writer = csv.DictWriter(csv_doc, fieldnames=COLUMN_NAMES)
+
+            for data in dict_rows:
+                csv_writer.writerow(data)
+
+    except IOError:
+        print('I/O Error occurred')
 
 
 def parse(page_num):
@@ -37,7 +54,7 @@ def parse(page_num):
     for link in list(zip(download_buttons, file_names)):
         if link[0].get_text() == 'View/Open':
             person_name = extract_name(soup)
-            print(f"parsing page about {person_name}, page: {page}")
+            print(f"parsing page about {person_name}, page: {page_num}")
 
             # create named directory
             make_dir(f"{DOWNLOADS}/{person_name}")
@@ -50,15 +67,18 @@ def parse(page_num):
 
     # extract metadata about master thesis if thesis is open to view
     if has_access:
+        # parse metadata
         labels = soup.find_all('td', {'class': 'metadataFieldLabel'})
         values = soup.find_all('td', {'class': 'metadataFieldValue'})
         for metadata in list(zip(labels, values)):
-            print(f"{metadata[0].get_text()} - {metadata[-1].get_text()}")
+            print(f"{metadata[0].get_text().strip()} - {metadata[-1].get_text().strip()}")
 
-        create_file(f"{DOWNLOADS}/data.csv")
-        with open(f"{DOWNLOADS}/data.csv", 'a') as doc:
-            print(doc)
-        #  fd.write(myCsvRow)
+        # parse statistics
+        for li in soup.find('div', {'id': 'statistics'}).ul.find_all('li'):
+            print(''.join(e.strip() for e in re.split(r'\d+', li.get_text())), li.span.get_text())
+
+        # parse abstract
+        print(soup.find('div', {'id': 'abstract'}).p.get_text())
     else:
         print('failed to download thesis, falling back to next page')
 
@@ -67,5 +87,12 @@ def parse(page_num):
     #     parse(page_num+1)
 
 
-# make_dir(DOWNLOADS)
-parse(6800)
+if not os.path.exists(f"{DOWNLOADS}/data.csv"):
+    create_file(f"{DOWNLOADS}/data.csv")
+    try:
+        with open(f"{DOWNLOADS}/data.csv", 'a') as doc:
+            writer = csv.DictWriter(doc, fieldnames=COLUMN_NAMES)
+            writer.writeheader()
+    except IOError:
+        print('I/O Error occurred')
+parse(1815)
