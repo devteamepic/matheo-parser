72967249080a246044f61d3983a4649d32c82f18
paydaylight <maxat235@gmail.com>
2020-05-16 11:49:45 +0600

    update

---
+++ check_csv.py
@@ -0,0 +1,11 @@
+import csv
+
+with open(f"downloads/data.csv", 'r') as csv_doc:
+    csv_reader = csv.reader(csv_doc)
+
+    written_pages = []
+    for row in csv_reader:
+        if row[0] != 'Page':
+            written_pages.append(row[0])
+
+print(written_pages)
--- parser.py
+++ parser.py
@@ -70,8 +70,6 @@ def append_row(dict_rows, csv_path=f"{DOWNLOADS}/data.csv"):
         print('I/O Error occurred')
 
 
-
-
 def parse(page_num, dict):
     try:
         page = urlopen(f"https://matheo.uliege.be/handle/2268.2/{page_num}")
@@ -131,6 +129,48 @@ def parse(page_num, dict):
         # iterate forward
 
 
+def parse_to_csv(page_num, dict):
+    try:
+        page = urlopen(f"https://matheo.uliege.be/handle/2268.2/{page_num}")
+    except URLError:
+        print("Something went wrong. falling back to the next page")
+        return
+
+    soup = BeautifulSoup(page, 'html.parser')
+    print(soup)
+    try:
+        person_name = extract_name(soup)
+    except:
+        return False
+
+    dict['Page'] = page_num
+    dict['Author'] = person_name
+
+    labels = soup.find_all('td', {'class': 'metadataFieldLabel'})
+    values = soup.find_all('td', {'class': 'metadataFieldValue'})
+
+    for metadata in list(zip(labels, values)):
+        label = metadata[0].get_text().replace(':', '').strip()
+        value = metadata[-1].get_text().strip()
+        dict[label] = value
+        # print(f"{label} - {value}")
+
+    # parse statistics
+    for li in soup.find('div', {'id': 'statistics'}).ul.find_all('li'):
+        label = ''.join(e.strip() for e in re.split(r'\d+', li.get_text()))
+        value = li.span.get_text()
+        dict[label] = value
+        # print(f"{label} - {value}")
+
+    # parse abstract
+    abstract = soup.find('div', {'id': 'abstract'}).p
+    if abstract is not None:
+        dict['Abstract'] = abstract.get_text()
+
+    grouped_dicts.append(dict)
+    append_row(new_data([dict]))
+
+
 def iterate_recursive(page_num):
     if page_num < 8500:
         parse(page_num, {})
@@ -140,6 +180,21 @@ def iterate_recursive(page_num):
 def iterate(page_num):
     while page_num <= 8500:
         parse(page_num, {})
+        page_num += 1
+
+
+def iterate_csv_only(page_num=1118):
+    with open(f"{DOWNLOADS}/data.csv", 'r') as csv_doc:
+        csv_reader = csv.reader(csv_doc)
+
+        written_pages = []
+        for row in csv_reader:
+            if row[0] != 'Page':
+                written_pages.append(row[0])
+
+    while page_num <= 8725:
+        if page_num not in written_pages:
+            parse_to_csv(page_num, {})
         page_num += 1
 
 
@@ -154,5 +209,6 @@ if not os.path.exists(f"{DOWNLOADS}/data.csv"):
 
 # counter = 0
 grouped_dicts = []
-iterate(1113)
-append_row(new_data(grouped_dicts))
+# iterate(1113)
+# append_row(new_data(grouped_dicts))
+iterate_csv_only()
